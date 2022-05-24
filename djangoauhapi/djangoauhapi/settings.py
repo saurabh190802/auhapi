14b7d914788ba87301355896b67eac710b841bc3
saurabh satapathy <saurabhsatapathy0@gmail.com>
2022-05-24 19:42:49 +0530

    Update settings.py

--- djangoauhapi/djangoauhapi/settings.py
+++ djangoauhapi/djangoauhapi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-68kz$*139w_tsc9&k2jhxymv2nolbg=%fj_mt#!fewukoe9fuk'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
